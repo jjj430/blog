a1ca57f7b6b197a917ecd77a50b3192364686623
Rosemary Joe <jjj430@msn.com>
2016-09-22 20:09:29 -0700

    add json file and ajax call

--- scripts/project.js
+++ scripts/project.js
@@ -8,15 +8,52 @@ function Project (opts) {
   this.dateComplete = opts.dateComplete;
 }
 
+Project.all = [];
+
 Project.prototype.toHtml = function() {
   var appTemplate = $("#template").html();
   var compileTemplate = Handlebars.compile(appTemplate);
   return compileTemplate(this);
 }
+Project.loadAll = function(rawData) {
+  rawData.forEach(function(ele) {  //creates a new object for each element of data
+    Project.all.push(new Project(ele));
+  });
+};
+Project.fetchAll = function() {
+  function processData(data){
+    Project.loadAll(data);
+    projectView.initIndexPage();
+  }
+  function fetchFromServer(){
+    $.getJSON('data/projectData.json', function (data) {
+      localStorage.setItem('rawData', JSON.stringify(data));
+      processData(data);
+    });
+  }
+  function fetchFromLocalStorage(){
+    var rawData=localStorage.getItem('rawData');
+    processData(JSON.parse(rawData));
+  }
+  $.ajax ({
+    url: 'data/projectData.json',
+    type: 'HEAD',
+    success: function(data, message, xhr){
+      var etag = xhr.getResponseHeader('ETag');
+      if(localStorage.etag){
+        var localEtag = localStorage.etag;
+        if(localEtag===etag && localStorage.rawData){
+          fetchFromLocalStorage();
+        } else {
+          fetchFromServer();
+        }
+      }else {fetchFromServer();
+        }
+        localStorage.setItem('etag', etag);
+    }
+  });
+}
 
-rawData.forEach(function(ele) {  //creates a new object for each element of data
-  projects.push(new Project(ele));
-})
 
 projects.forEach(function(a){
   $('#projects').append(a.toHtml())
